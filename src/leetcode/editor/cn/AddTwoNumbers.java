ee7f8a963b6aca0731c0d81d59e91121069f5eb1
yangyi <yangyi0907@mail.ustc.edu.cn>
2023-01-14 12:47:58 +0800

    11

---
+++ src/leetcode/editor/cn/AddTwoNumbers.java
@@ -0,0 +1,25 @@
+package leetcode.editor.cn;
+//2.两数相加
+public class AddTwoNumbers {
+ public static void main(String[] args) {
+  Solution solution = new AddTwoNumbers().new Solution();
+ }
+ //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/LongestCommonPrefix .java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn;
+
+/**
+ * 最长公共前缀
+ * @author DY YANGYI
+ * @date 2023-01-14 12:43:52
+ */
+public class LongestCommonPrefix{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new LongestCommonPrefix().new Solution();
+	 }
+	 
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+		return null;
+	}
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
